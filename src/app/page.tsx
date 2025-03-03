24cb71cb42715bc1a67b94bac037a810047f1a08
essex999 <oktav2222@gmail.com>
2025-03-03 11:07:22 +0100

    header fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import brendLogoIcon from '@/assets/images/brendLogoIcon.png'
 import phoneIcon from '@/assets/images/phoneIcon.png'
 import { phoneNumber } from '@/constants/basicConsts'
 import Image from 'next/image'
@@ -17,8 +18,26 @@ export default function Home() {
 				</div>
 				<div className={sty.languageSelect}>
 					<button>{'DE'}</button>
+
 					<button>{'RO'}</button>
+
 					<button>{'RU'}</button>
+				</div>
+			</div>
+			<div className={sty.secondHeader}>
+				<div className={sty.siteNameContainer}>
+					<div className={sty.siteName}>
+						<p>AlcoExpress</p>
+						<Image
+							src={brendLogoIcon}
+							alt='brendLogoIcon'
+							className={sty.brendLogoIcon}
+						></Image>
+					</div>
+					<p>Wir bringen den Genuss zu Ihnen nach Hause!</p>
+				</div>
+				<div>
+					<input type='text' placeholder='Search in katalog' />
 				</div>
 			</div>
 		</div>
