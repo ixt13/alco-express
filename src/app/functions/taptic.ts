1e4f95f42256f1ed47d17e7905babf1fbc6c6b8c
Ceban Octavian <oktav2222@gmail.com>
2025-05-16 22:11:49 +0200

    fx

--- src/app/functions/taptic.ts
+++ src/app/functions/taptic.ts
@@ -1,5 +1,9 @@
 export const taptic = (type: 'success' | 'error' | 'warning') => {
-	if (typeof window !== 'undefined' && 'navigator' in window) {
+	if (
+		typeof window !== 'undefined' &&
+		'navigator' in window &&
+		'vibrate' in window.navigator
+	) {
 		const navigator = window.navigator
 		if (navigator.vibrate) {
 			switch (type) {
