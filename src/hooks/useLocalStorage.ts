154c03fc8f6d5e8592d2d9b33e3f212b0995991c
Ceban Octavian <oktav2222@gmail.com>
2025-08-17 21:06:35 +0300

    fx

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,13 @@
+import { useEffect, useState } from 'react'
+
 export const useLocalStorage = () => {
-	const favoritesList = localStorage.getItem('favoritesList')
+	const [favoritesList, setFavoritesList] = useState<[] | null>(null)
+
+	useEffect(() => {
+		const items = localStorage.getItem('favoritesList')
+
+		setFavoritesList(items ? JSON.parse(items) : [])
+	}, [])
 
 	return { favoritesList }
 }
